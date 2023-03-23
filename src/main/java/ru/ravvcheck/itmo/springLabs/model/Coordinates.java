579648716bcccb58774562a45c78c329521ea159
Marik368418 <117970735+Marik368418@users.noreply.github.com>
2023-03-23 13:10:17 +0300

    getters and setters

--- src/main/java/ru/ravvcheck/itmo/springLabs/model/Coordinates.java
+++ src/main/java/ru/ravvcheck/itmo/springLabs/model/Coordinates.java
@@ -3,4 +3,20 @@ package ru.ravvcheck.itmo.springLabs.model;
 public class Coordinates {
     private Integer x; //Поле не может быть null
     private Double y; //Значение поля должно быть больше -903, Поле не может быть null
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Double getY() {
+        return y;
+    }
+
+    public void setY(Double y) {
+        this.y = y;
+    }
 }
--- src/main/java/ru/ravvcheck/itmo/springLabs/model/SpaceMarine.java
+++ src/main/java/ru/ravvcheck/itmo/springLabs/model/SpaceMarine.java
@@ -1,5 +1,7 @@
 package ru.ravvcheck.itmo.springLabs.model;
 
+import java.time.LocalDate;
+
 public class SpaceMarine {
     private long id; //Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
@@ -10,4 +12,76 @@ public class SpaceMarine {
     private String achievements; //Поле не может быть null
     private AstartesCategory category; //Поле может быть null
     private Chapter chapter; //Поле не может быть null
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Long getHealth() {
+        return health;
+    }
+
+    public void setHealth(Long health) {
+        this.health = health;
+    }
+
+    public long getHeartCount() {
+        return heartCount;
+    }
+
+    public void setHeartCount(long heartCount) {
+        this.heartCount = heartCount;
+    }
+
+    public String getAchievements() {
+        return achievements;
+    }
+
+    public void setAchievements(String achievements) {
+        this.achievements = achievements;
+    }
+
+    public AstartesCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(AstartesCategory category) {
+        this.category = category;
+    }
+
+    public Chapter getChapter() {
+        return chapter;
+    }
+
+    public void setChapter(Chapter chapter) {
+        this.chapter = chapter;
+    }
 }
