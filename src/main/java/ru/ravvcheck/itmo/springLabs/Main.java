56f61c652194bb700684db592a56b6e6d60d0145
Marik368418 <117970735+Marik368418@users.noreply.github.com>
2023-03-26 14:23:53 +0300

    test pushing

--- src/main/java/ru/ravvcheck/itmo/springLabs/Main.java
+++ src/main/java/ru/ravvcheck/itmo/springLabs/Main.java
@@ -2,7 +2,7 @@ package ru.ravvcheck.itmo.springLabs;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("q");
+        System.out.println("q1");
 
     }
 }
