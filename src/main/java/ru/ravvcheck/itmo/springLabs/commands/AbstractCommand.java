72a29ed535c4c66ef0aab2265d46e22eea74d4a0
Marik368418 <117970735+Marik368418@users.noreply.github.com>
2023-03-26 14:35:15 +0300

    hot fix: replace TreeSet<String, String> to String[] args in execute

--- src/main/java/ru/ravvcheck/itmo/springLabs/commands/AbstractCommand.java
+++ src/main/java/ru/ravvcheck/itmo/springLabs/commands/AbstractCommand.java
@@ -3,5 +3,5 @@ package ru.ravvcheck.itmo.springLabs.commands;
 import java.util.TreeSet;
 
 public abstract class AbstractCommand {
-    public abstract void execute(TreeSet<String, String> args);
+    public abstract void execute(String[] args);
 }
